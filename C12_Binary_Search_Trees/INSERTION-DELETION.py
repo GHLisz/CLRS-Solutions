45473b89a183f4846d3d9662f8e168cee13ae167
GHLisz <leeshzh@outlook.com>
2017-08-22 22:29:50 +0800

    tree deletion

--- C12_Binary_Search_Trees/INSERTION-DELETION.py
+++ C12_Binary_Search_Trees/INSERTION-DELETION.py
@@ -35,8 +35,46 @@ def tree_insert(T, z):
     return T
 
 
-def to_list(T):
-    root = T.root
+def transplant(T, u, v):
+    if u.p is None:
+        T.root = v
+    elif u is u.p.left:
+        u.p.left = v
+    else:
+        u.p.right = v
+    if v is not None:
+        v.p = u.p
+
+
+def tree_delete(T, z):
+    if z.left is None:
+        transplant(T, z, z.right)
+    elif z.right is None:
+        transplant(T, z, z.left)
+    else:
+        y = tree_minimum(z.right)
+        if y.p is not z:
+            transplant(T, y, y.right)
+            y.right = z.right
+            y.right.p = y
+        transplant(T, z, y)
+        y.left = z.left
+        y.left.p = y
+
+
+def tree_minimum(x):
+    while x.left is not None:
+        x = x.left
+    return x
+
+
+def tree_maximum(x):
+    while x.right is not None:
+        x = x.right
+    return x
+
+
+def to_list(root):
     if root is None:
         return []
     return to_list(root.left) + [root.key] + to_list(root.right)
@@ -45,15 +83,16 @@ def to_list(T):
 class ProblemTestCase(unittest.TestCase):
 
     def test_case(self):
-        root = Tree(root=TreeNode(3))
-        # root = tree_insert(None, TreeNode(3))
-        root = tree_insert(root, TreeNode(1))
-        root = tree_insert(root, TreeNode(5))
-        root = tree_insert(root, TreeNode(0))
-        root = tree_insert(root, TreeNode(2))
-        root = tree_insert(root, TreeNode(4))
-        root = tree_insert(root, TreeNode(6))
-        self.assertEqual(to_list(root), [0, 1, 2, 3, 4, 5, 6])
+        T = tree_insert(Tree(), TreeNode(3))
+        T = tree_insert(T, TreeNode(1))
+        T = tree_insert(T, TreeNode(5))
+        T = tree_insert(T, TreeNode(0))
+        T = tree_insert(T, TreeNode(2))
+        T = tree_insert(T, TreeNode(4))
+        T = tree_insert(T, TreeNode(6))
+        self.assertEqual(to_list(T.root), [0, 1, 2, 3, 4, 5, 6])
+        tree_delete(T, T.root.left)
+        self.assertEqual(to_list(T.root), [0, 2, 3, 4, 5, 6])
 
 
 if __name__ == '__main__':
