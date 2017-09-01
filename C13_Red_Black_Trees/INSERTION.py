b45c418096790f46dfaea6db8c968561e486971c
GHLisz <leeshzh@outlook.com>
2017-09-01 23:09:12 +0800

    rbtree insertion

--- C13_Red_Black_Trees/INSERTION.py
+++ C13_Red_Black_Trees/INSERTION.py
@@ -1,4 +1,6 @@
 RED = True
+BLACK = False
+
 
 def rb_insert(T, z):
     y = T.nil
@@ -23,4 +25,26 @@ def rb_insert_fixup(T, z):
     while z.p.color == RED:
         if z.p == z.p.p.left:
             y = z.p.p.right
+            if y.color == RED:
+                z.p.color = BLACK
+                y.color = BLACK
+                z.p.p.color = RED
+                z = z.p.p
+            elif z == z.p.p.right:
+                z = z.p
+                left_rotate(T, z)
+                z.p.color = BLACK
+                z.p.p.color = RED
+                right_rotate(T, z.p.p)
+        else:
             pass
+
+
+
+def left_rotate(T, z):
+    pass
+
+
+def right_rotate(T, z):
+    pass
+
