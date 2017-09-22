ad1802f40977ad33cec01b9f80961257384099af
GHLisz <leeshzh@outlook.com>
2017-09-22 22:58:50 +0800

    rb tree

--- C13_Red_Black_Trees/DELETION.py
+++ C13_Red_Black_Trees/DELETION.py
@@ -24,3 +24,40 @@ def rb_transplant(T, u, v):
     else:
         u.p.right = v
     v.p = u.p
+
+
+def tree_minimum(x):
+    while x.left is not None:
+        x = x.left
+    return x
+
+
+def rb_delete(T, z):
+    y = z
+    y_original_color = y.color
+    if z.left == T.nil:
+        x = z.right
+        rb_transplant(T, z, z.right)
+    elif z.right == T.nil:
+        x = z.left
+        rb_transplant(T, z, z.right)
+    else:
+        y = tree_minimum(z.right)
+        y_original_color = y.color
+    x = y.right
+    if y.p == z:
+        x.p = y
+    else:
+        rb_transplant(T, y, y.right)
+        y.right = z.right
+        y.right.p = y
+    rb_transplant(T, z, y)
+    y.left = z.left
+    y.left.p = y
+    y.color = z.color
+    if y_original_color == BLACK:
+        rb_delete_fixup(T, x)
+
+
+def rb_delete_fixup(T, x):
+    pass
