70a47fe4b8c757a9ccd71823a1fc67b92f9ef78f
GHLisz <leeshzh@outlook.com>
2017-10-06 22:42:32 +0800

    c14

--- C14_Augmenting_Data_Structures/ex_14.1-4.py
+++ C14_Augmenting_Data_Structures/ex_14.1-4.py
@@ -1,11 +1,7 @@
-def os_key_rank(x, k, i=0):
-    r = 1
-    if x.left is not None:
-        r += x.left.size
-
+def os_key_rank(x, k):
     if k == x.key:
-        return i + r
-    if k < x.key:
-        return os_key_rank(x.left, k, i)
-    if k > x.key:
-        return os_key_rank(x.right, k, i+r)
+        return x.left.size + 1
+    elif k < x.key:
+        return os_key_rank(x.left, k)
+    else:
+        return os_key_rank(x.right, k) + x.left.size + 1
