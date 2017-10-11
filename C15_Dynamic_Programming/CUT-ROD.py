b3943b7b24a0f2d2f66fc8175f528205610a1bf2
GHLisz <leeshzh@outlook.com>
2017-10-11 22:53:24 +0800

    cut-rod

--- C15_Dynamic_Programming/CUT-ROD.py
+++ C15_Dynamic_Programming/CUT-ROD.py
@@ -8,3 +8,33 @@ def cut_rod(p, n):
     for i in range(1, n+1):
         q = max(q, p[i] + cut_rod(p, n-i))
     return q
+
+
+def memoized_cut_rod(p, n):
+    r = [-math.inf for _ in range(0, n+1)]
+    return memoized_cut_rod_aux(p, n, r)
+
+
+def memoized_cut_rod_aux(p, n, r):
+    if r[n] >= 0:
+        return r[n]
+
+    if n == 0:
+        q = 0
+    else:
+        q = -math.inf
+        for i in range(1, n+1):
+            q = max(q, p[i] + memoized_cut_rod_aux(p, n-i, r))
+    r[n] = q
+    return q
+
+
+def bottom_up_cut_rod(p, n):
+    r = [-math.inf for _ in range(0, n+1)]
+    r[0] = 0
+    for j in range(1, n+1):
+        q = -math.inf
+        for i in range(1, j+1):
+            q = max(q, p[i] + r[j-i])
+        r[j] = q
+    return r[n]
